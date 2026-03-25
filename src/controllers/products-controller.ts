a39dfa34c2631e562f387c33f9a0b17d828ba2eb
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-03-25 12:43:00 -0300

    feat(controller): add remove method for products with validation and error handling

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { knexInstance as knex } from "@/database/knex"
+import { AppError } from "@/utils/AppError"
 import { z } from "zod"
 
 export class ProductController {
@@ -43,7 +44,31 @@ export class ProductController {
 
             const { name, price } = bodySchema.parse(request.body)
 
+            const product = await knex<ProductRepository>("products").select().where({ id }).first()
+
+            if (!product) {
+                throw new AppError("product not found")
+            }
+
             await knex<ProductRepository>("products").update({ name, price, updated_at: knex.fn.now() }).where({ id })
+
+            return response.json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async remove(request: Request, response: Response, next: NextFunction) {
+        try {
+            const id = z.string().transform((value) => Number(value)).refine((value) => !isNaN(value), { message: "id must be a number" }).parse(request.params.id)
+
+            const product = await knex<ProductRepository>("products").select().where({ id }).first()
+
+            if (!product) {
+                throw new AppError("product not found")
+            }
+
+            await knex<ProductRepository>("products").delete().where({ id })
 
             return response.json()
         } catch (error) {
