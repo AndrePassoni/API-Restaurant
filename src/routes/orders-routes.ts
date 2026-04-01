e42dc402dd0b8559d7ff2a4f856a1cb9103dab12
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-04-01 10:33:37 -0300

    feat(orders): add show endpoint to retrieve total price and quantity by table session ID

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -6,3 +6,4 @@ const ordersController = new OrdersController
 
 ordersRoutes.post("/", ordersController.create)
 ordersRoutes.get("/table-session/:table_session_id", ordersController.index)
+ordersRoutes.get("/table-session/:table_session_id/total", ordersController.show)
